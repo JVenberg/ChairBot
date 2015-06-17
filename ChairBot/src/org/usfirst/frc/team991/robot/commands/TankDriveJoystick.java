c9cdf7481be3d235ba0c9180b22b4558c6efe05f
Jack Venberg <jvenberg@gmail.com>
2015-06-17 13:23:16 -0700

    Changed Controller and Inverted

--- ChairBot/src/org/usfirst/frc/team991/robot/commands/TankDriveJoystick.java
+++ ChairBot/src/org/usfirst/frc/team991/robot/commands/TankDriveJoystick.java
@@ -20,7 +20,7 @@ public class TankDriveJoystick extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.drivetrain.tankDrive(Robot.oi.getLeftJoy(), Robot.oi.getRightJoy(), true);
+    	Robot.drivetrain.tankDrive(Robot.oi.getLeftJoy().getY(), Robot.oi.getLeftJoy().getTwist());
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- ChairBot/src/org/usfirst/frc/team991/robot/subsystems/DriveTrain.java
+++ ChairBot/src/org/usfirst/frc/team991/robot/subsystems/DriveTrain.java
@@ -29,6 +29,11 @@ public class DriveTrain extends Subsystem {
 		back_right_motor = new Talon(RobotMap.backrightMotor);
 		drive = new RobotDrive(front_left_motor, back_left_motor, front_right_motor, back_right_motor);
 		
+		drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+		drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+		drive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+		drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+		
 		LiveWindow.addActuator("Drive Train", "Left Motor", (Talon) front_left_motor);
 		LiveWindow.addActuator("Drive Train", "Right Motor", (Talon) front_right_motor);
 		LiveWindow.addActuator("Drive Train", "Left Motor", (Talon) back_left_motor);
@@ -42,14 +47,20 @@ public class DriveTrain extends Subsystem {
     	setDefaultCommand(new TankDriveJoystick());
     }
     
-    public void tankDrive(double left, double right) {
-		drive.tankDrive(left, right);
+    public void tankDrive(double y, double rot) {
+    	if(y <= 0) {
+        	y = y * .45;
+    	} else {
+    		y = y * .75;
+    	}
+    	rot = rot * .6;
+		drive.arcadeDrive(y, rot, true);
 	}
 
 
-	public void tankDrive(Joystick leftJoy, Joystick rightJoy, boolean squared) {
-		drive.tankDrive(leftJoy, rightJoy, squared);
-	}
+//	public void tankDrive(Joystick leftJoy, Joystick rightJoy, boolean squared) {
+//		drive.tankDrive(leftJoy, rightJoy, squared);
+//	}
 	public void stop() {
 		drive.tankDrive(0, 0);
 	}
