3485b28acf7e27376f099a0b9874eff68d98bfea
JackVenberg <jvenberg@gmail.com>
2015-07-10 00:18:09 -0700

    Added Turn command

---
+++ ChairBot/src/org/usfirst/frc/team991/robot/commands/Turn.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team991.robot.commands;
+
+import org.usfirst.frc.team991.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class Turn extends Command {
+	private double angleOfTurn, currentAngle;
+
+    public Turn(double angleOfTurn, double timeout) {
+        requires(Robot.drivetrain);
+        setTimeout(timeout);
+        this.angleOfTurn = angleOfTurn;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.drivetrain.resetGyroAngle();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	currentAngle = Robot.drivetrain.getGyroAngle();
+    	Robot.drivetrain.turn(angleOfTurn - currentAngle);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.drivetrain.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
