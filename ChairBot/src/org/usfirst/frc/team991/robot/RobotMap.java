dd1f104eadd8d0135864db160d03c9a0c7df323f
JackVenberg <jvenberg@gmail.com>
2015-07-09 23:55:15 -0700

    Updated RobotMap for gyro

--- ChairBot/src/org/usfirst/frc/team991/robot/RobotMap.java
+++ ChairBot/src/org/usfirst/frc/team991/robot/RobotMap.java
@@ -12,10 +12,12 @@ public class RobotMap {
     public static int backrightMotor = 2;
     public static int frontrightMotor = 3;
     
-    public static int leftencoderAChannel = 1;
-    public static int leftencoderBChannel = 0;
-    public static int rightencoderAChannel = 3;
-    public static int rightencoderBChannel = 2;
+    public static int leftencoderAChannel = 2;
+    public static int leftencoderBChannel = 1;
+    public static int rightencoderAChannel = 4;
+    public static int rightencoderBChannel = 3;
+    
+    public static int gyro = 0;
     
     // Shooter
     public static int loadersolenoid = 0;
