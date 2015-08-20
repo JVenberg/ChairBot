2a6fb2c69e827d233c560b5b6539ec5cd9c066f2
JackVenberg <jvenberg@gmail.com>
2015-08-20 14:59:21 -0700

    Simplified Camera

--- ChairBot/src/org/usfirst/frc/team991/robot/Robot.java
+++ ChairBot/src/org/usfirst/frc/team991/robot/Robot.java
@@ -4,7 +4,6 @@ package org.usfirst.frc.team991.robot;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -44,8 +43,9 @@ public class Robot extends IterativeRobot {
 	public SendableChooser autonomousDirectionChooser;
 	
 	//Camera Server
-    int session;
-    Image frame;
+//    int session;
+//    Image frame;
+	CameraServer camera;
 	
 	public void robotInit() {
 		//Gets Preferences
@@ -68,12 +68,13 @@ public class Robot extends IterativeRobot {
 		pneumatics.start();
 		
 		//Camera Server
-		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
-
-        // the camera name (ex "cam0") can be found through the roborio web interface
-        session = NIVision.IMAQdxOpenCamera("cam0",
-                NIVision.IMAQdxCameraControlMode.CameraControlModeController);
-        NIVision.IMAQdxConfigureGrab(session);
+//		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+//		session = NIVision.IMAQdxOpenCamera("cam0",
+//        		NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+//		NIVision.IMAQdxConfigureGrab(session);
+		camera = CameraServer.getInstance();
+		camera.setQuality(25);
+		camera.startAutomaticCapture("cam0");
 		
 		//SmartDashboard Data
 		SmartDashboard.putData(Scheduler.getInstance());
@@ -112,25 +113,12 @@ public class Robot extends IterativeRobot {
 	 */
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
-		NIVision.IMAQdxStartAcquisition(session);
-
-        /**
-         * grab an image, draw the circle, and provide it for the camera server
-         * which will in turn send it to the dashboard.
-         */
-        //NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
-
-
-        NIVision.IMAQdxGrab(session, frame, 1);
-        //NIVision.imaqDrawShapeOnImage(frame, frame, rect,
-        //        DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
-        
-        CameraServer.getInstance().setImage(frame);
-
-        /** robot code here! **/
-        Timer.delay(0.005);		// wait for a motor update time
-        
-        NIVision.IMAQdxStopAcquisition(session);
+		
+		//Camera Periodic
+//		NIVision.IMAQdxStartAcquisition(session);
+//		NIVision.IMAQdxGrab(session, frame, 1);
+//		CameraServer.getInstance().setImage(frame);
+//		NIVision.IMAQdxStopAcquisition(session);
 	}
 	
 	/**
